0cd8f5ff9433db8df89f715372f3aa57230f8b4b
Kshitij Kumar <1234567890kshitij@gmail.com>
2021-08-13 16:12:08 +0530

    first commit

---
+++ helpers/itemList.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    name: "Chips",
+    category: "Food and Beverages",
+    available_at: [
+      {
+        address: "24x7 Sector 54, Gurgaon",
+        lat: 12.21,
+        long: 28.72,
+      },
+      {
+        address: "Big Bazaar, Sector 25, Gurgaon",
+        lat: 12.23,
+        long: 28.79,
+      },
+    ],
+  },
+  {
+    name: "Cold Drink",
+    category: "Food and Beverages",
+    available_at: [
+      {
+        address: "24x7 Sector 54, Gurgaon",
+        lat: 12.21,
+        long: 28.72,
+      },
+      {
+        address: "Big Bazaar, Sector 25, Gurgaon",
+        lat: 12.23,
+        long: 28.79,
+      },
+    ],
+  },
+];
